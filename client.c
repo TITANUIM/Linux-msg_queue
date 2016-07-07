97ef4d4fc0fe263ec67f827057c1a5cd5783e5c6
TITANUIM <TITANUIM@users.noreply.github.com>
2016-07-07 22:28:49 +0800

    Update client.c

--- client.c
+++ client.c
@@ -4,6 +4,44 @@
 
 int main()
 {
+	#include"comm.h"
+
+int main()
+{
+	//int msg_id = creat_msg_queue();
+	
+	//if(msg_id <0)
+	//{
+	//	printf("%d : %s\n",errno,strerror(errno));
+	//	return 1;
+	//}
+	//
+	//
+	int msg_id = get_msg_queue();
+
+	char buf[_SIZE_];
+	while(1)
+	{
+		printf("Please Enter:");
+		fflush(stdout);
+		ssize_t _s = read(0,buf,sizeof(buf)-1);
+		if(_s >0 )
+		{
+			buf[_s - 1] = '\0';
+		}
+		send_msg(msg_id,client_type,buf);
+		if(strcasecmp(buf,"quit") == 0)
+		{
+			break;
+		}
+
+		memset(buf,'\0',sizeof(buf));
+		recv_msg(msg_id,server_type,buf);
+		printf("server# %s\n",buf);
+	}
+
+	return 0;
+}
 
 	int msg_id = get_msg_queue();
 
