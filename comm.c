b3c06f2cedaa419d24c73bd8852eefa91dac6e4c
TITANUIM <TITANUIM@users.noreply.github.com>
2016-07-07 22:24:11 +0800

    Create comm.c

---
+++ comm.c
@@ -0,0 +1,75 @@
+//对各个接口进行定义
+
+#include"comm.h"
+
+int server_type = 1;
+int client_type = 2;
+
+
+static int comm_msg_queue(int flags)
+{
+	key_t _key = ftok(_PATH_NAME_,_PROJ_ID_);
+	if(_key < 0)
+	{
+		printf("%d : %s",errno,strerror(errno));
+		return -1;
+	}
+
+	int msg_id = msgget(_key,flags);
+	//int msg_id = msgget(_key,IPC_CREAT | IPC_EXCL | 0666);
+	return msg_id;
+}
+
+int creat_msg_queue()
+{
+	int flags = IPC_CREAT | IPC_EXCL | 0666;
+	return comm_msg_queue(flags);
+}
+
+int get_msg_queue()
+{
+	int flags = IPC_CREAT;
+	return comm_msg_queue(flags);
+}
+
+int destroy_queue(int msg_id)
+{
+	if(msgctl(msg_id,IPC_RMID,NULL) != 0)
+	{
+		printf("%d : %s",errno,strerror(errno));
+		return -1;
+	}
+
+	return 0;
+}
+
+int send_msg(int msg_id,int send_type,const char* msg)
+{
+	struct msgbuf _buf;
+	_buf.mtype = send_type;
+	strncpy(_buf.mtext,msg,strlen(msg)+1);
+
+	if(msgsnd(msg_id,&_buf,sizeof(_buf.mtext),0) < 0)
+	{
+		printf("%d : %s",errno,strerror(errno));
+	    return -1;
+	}
+	return 0;
+}
+
+int recv_msg(int msg_id,int recv_type,char* msg_out)
+{
+	struct msgbuf _buf;
+	_buf.mtype = 0;
+	memset(_buf.mtext,'\0',sizeof(_buf.mtext));
+
+	if(msgrcv(msg_id,&_buf,sizeof(_buf.mtext),recv_type,0) < 0)
+	{
+		printf("%d : %s",errno,strerror(errno));
+	    return -1;
+	}
+
+	strcpy(msg_out,_buf.mtext);
+	return 0;
+}
+
